22a3033624ef5e6cd404f5909e4fe1c04d38a42b
Juan Gonon <juanfegonon27@gmail.com>
2024-09-16 10:07:20 -0600

    expres and rutas

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('calendar backend')
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+// rutas
+
+app.get('/', (req, res) => {
+    console.log(req.url)
+    res.json({
+        ok: true
+    })
+})
+
+
+// Escuchar peticiones
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`)
+})
