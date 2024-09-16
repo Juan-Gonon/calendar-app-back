7758d694eed2c028773ac15dcd50b38930be5f56
Juan Gonon <juanfegonon27@gmail.com>
2024-09-16 10:19:30 -0600

    variables de entorno

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
-const port = 4000
+const port = process.env.PORT
 
 // dir public
 app.use(express.static('public'))
