b1c6a57aac630d1e7738831166bdb8c0c6b6ba74
Juan Gonon <juanfegonon27@gmail.com>
2024-09-17 09:24:18 -0600

    customMiddleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,16 +5,6 @@ const createUser = (req = request, res = response) => {
     // console.log(req.url)
     const { name, email, password } = req.body
 
-    const errors = validationResult(req)
-    console.log(errors)
-
-    if( !errors.isEmpty() ){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -25,14 +15,6 @@ const createUser = (req = request, res = response) => {
 }
 
 const loginUser = (req = request, res = response) => {
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
     const { email, password } = req.body
 
     res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,22 @@
  */
 const { Router } = require('express')
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos')
 const router = Router()
 const { createUser, loginUser, revalidateToke } = require('../controllers/auth')
 
 router.post('/',[
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password debe de ser 6 caracteres').isLength({ min: 6})
+    check('password', 'El password debe de ser 6 caracteres').isLength({ min: 6}),
+    validarCampos
 ],loginUser )
 
 router.post('/new', [
     // middlewares
     check('name', 'El nombre es obligatorio').notEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password debe de ser 6 caracteres').isLength({min: 6})
+    check('password', 'El password debe de ser 6 caracteres').isLength({min: 6}),
+    validarCampos
 ] , createUser )
 
 router.get('/renew', revalidateToke )
