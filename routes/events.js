cccaf0331a95cc45b3013ad17106e8392d49cd17
Juan Gonon <juanfegonon27@gmail.com>
2024-09-20 09:38:00 -0600

    isDate, custom and moment

--- routes/events.js
+++ routes/events.js
@@ -6,7 +6,10 @@
  */
 
 const { Router  } = require('express')
+const { check } = require('express-validator')
 const { validarJWT } = require('../middlewares/validar-jwt')
+const { validarCampos } = require('../middlewares/validar-campos')
+const { isDate } = require('../helpers/isDate')
 const { getEvents, createEvents, updateEvents, deleteEvents } = require('../controllers/events')
 
 const router = Router()
@@ -14,7 +17,13 @@ const router = Router()
 router.use(validarJWT)
 
 router.get('/' ,getEvents)
-router.post('/' ,createEvents)
+router.post('/', [
+    check('title', 'El titulo es obligatorio').notEmpty(),
+    check('start', 'Fecha de inicio es obligatorio').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validarCampos
+
+] ,createEvents)
 router.put('/:id' ,updateEvents)
 router.delete('/:id' ,deleteEvents)
 
